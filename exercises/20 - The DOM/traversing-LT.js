bdd2b3db3e242f2c500f4e11293d73966163b5a3
Leanne <leanne.thng@gmail.com>
2020-01-03 15:58:53 -0800

    Create traversing-LT.js

---
+++ exercises/20 - The DOM/traversing-LT.js
@@ -0,0 +1,11 @@
+console.log(`it works`);
+// difference between node and element
+const wes = document.querySelector(`.wes`);
+console.log(wes.childNodes);
+
+console.log(wes.children);
+console.log(wes.firstElementChild);
+console.log(wes.lastElementChild);
+console.log(wes.previousElementSibling);
+console.log(wes.nextElementSibling);
+console.log(wes.parentElement);
