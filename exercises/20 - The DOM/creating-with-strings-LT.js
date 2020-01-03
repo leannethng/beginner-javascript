c572bad28e6fe6830a7928361b770c2505bbcf28
Leanne <leanne.thng@gmail.com>
2020-01-03 09:39:58 -0800

    Turn into DOM nodes before added to DOM
    Use .createContextualFragment to turn strings into DOM nodes before it is appended to the DOM

--- exercises/20 - The DOM/creating-with-strings-LT.js
+++ exercises/20 - The DOM/creating-with-strings-LT.js
@@ -2,10 +2,24 @@ const item = document.querySelector('.item');
 
 const src = 'https://picsum.photos/500';
 const myHtml = `
-<div> 
+<div class="wrapper"> 
 <h1>Hey how are ya! </h1>
 <img src ="${src}">
 </div>
 `;
-item.innerHTML = myHtml;
-console.log(item.innerHTML);
+// item.innerHTML = myHtml;
+// console.log(item.innerHTML);
+
+// const itemImage = document.querySelector('.wrapper img');
+
+// itemImage.classList.add('round');
+
+// console.log(itemImage);
+
+// turn a string into a dom element
+const myFragment = document.createRange().createContextualFragment(myHtml);
+
+console.log(myFragment.querySelector('img'));
+console.log(myFragment);
+
+document.body.appendChild(myFragment);
