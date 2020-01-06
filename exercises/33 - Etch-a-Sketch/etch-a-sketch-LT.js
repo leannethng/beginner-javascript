b3b6b75a1527b9a1ca24bff6979f00bc2dcdb2e3
Leanne <leanne.thng@gmail.com>
2020-01-05 16:30:14 -0800

    Shake button clears the canvas
    - Need to figure out how to change the starting point after clearing

--- exercises/33 - Etch-a-Sketch/etch-a-sketch-LT.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch-LT.js
@@ -1,15 +1,20 @@
+// Defining all the variables
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
 console.log(shakeButton);
+// Define X and Y coordinate starting points
 let x = 150;
 let y = 100;
+// Define how long the line is that is drawn
+const lineLength = 60;
 
-ctx.lineWidth = 50;
+// setting up canvas draw
+// Line Features
 // ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-// setting up canvas draw
-ctx.beginPath(x, y);
+ctx.lineWidth = lineLength;
+ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
@@ -22,20 +27,19 @@ window.addEventListener('keydown', function(e) {
   ctx.moveTo(x, y);
   switch (e.key) {
     case 'ArrowLeft':
-      x += -10;
+      x += -lineLength;
       e.preventDefault();
-      ß;
       break;
     case 'ArrowRight':
-      x += 10;
+      x += lineLength;
       e.preventDefault();
       break;
     case 'ArrowUp':
-      y += -10;
+      y += -lineLength;
       e.preventDefault();
       break;
     case 'ArrowDown':
-      y += 10;
+      y += lineLength;
       e.preventDefault();
       break;
     default:
@@ -43,4 +47,16 @@ window.addEventListener('keydown', function(e) {
   }
   ctx.lineTo(x, y);
   ctx.stroke();
+});
+
+// clear the canvas
+//
+shakeButton.addEventListener('click', function(e) {
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // ctx.moveTo(150, 150);
+  // ctx.lineTo(150, 150);
+
+  ctx.restore();
 });
