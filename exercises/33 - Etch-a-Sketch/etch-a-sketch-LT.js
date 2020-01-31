6709ff9eaaeda0530e8f165bf95c3cf8c9598180
Leanne <leanne.thng@gmail.com>
2020-01-30 21:25:20 -0800

    refactor: Refactored code - cleaned up the code
    Lessons Learnt:
    
    - Event listeners should have functions passed into them for cleanliness
    
    - Learnt about object destructuring
    
    `const width = canvas.width;` is the same as`const { width } = canvas;`
    
    You can have multiple properties passed though like below too:
    `const { width, height } = canvas;`

--- exercises/33 - Etch-a-Sketch/etch-a-sketch-LT.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch-LT.js
@@ -7,19 +7,23 @@ console.log(shakeButton);
 // let x = 150;
 // let y = 100;
 // Define how long the line is that is drawn
-const lineLength = 60;
+const LINE_WIDTH = 50;
+const MOVE_AMOUNT = 50;
 
+let hue = 0;
+
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 // setting up canvas draw
 // Line Features
-const myGradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-myGradient.addColorStop(0, 'red');
-myGradient.addColorStop(0.14, 'orange');
-myGradient.addColorStop(0.28, 'yellow');
-myGradient.addColorStop(0.42, 'green');
-myGradient.addColorStop(0.56, 'blue');
-myGradient.addColorStop(0.7, 'indigo');
-myGradient.addColorStop(0.84, 'violet');
-myGradient.addColorStop(1, 'red');
+// const myGradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+// myGradient.addColorStop(0, 'red');
+// myGradient.addColorStop(0.14, 'orange');
+// myGradient.addColorStop(0.28, 'yellow');
+// myGradient.addColorStop(0.42, 'green');
+// myGradient.addColorStop(0.56, 'blue');
+// myGradient.addColorStop(0.7, 'indigo');
+// myGradient.addColorStop(0.84, 'violet');
+// myGradient.addColorStop(1, 'red');
 // ctx.strokeStyle = myGradient;
 
 // destructuring
@@ -33,49 +37,69 @@ let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 // ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = lineLength;
+ctx.lineWidth = LINE_WIDTH;
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
-// add event listener for keys pressed
-// when key pressed do a thing - draw a line in the direction
-window.addEventListener('keydown', function(e) {
-  console.log(e.key);
-  ctx.beginPath(x, y);
+// destructured to just pass in .key with {key}
+function draw({ key }) {
+  // increment the hue
+
+  hue += 3;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  console.log(hue);
+  ctx.beginPath();
   ctx.moveTo(x, y);
-  switch (e.key) {
+  // move our x and y values depending on what the user did
+  switch (key) {
     case 'ArrowLeft':
-      x += -lineLength;
-      e.preventDefault();
+      x += -MOVE_AMOUNT;
       break;
     case 'ArrowRight':
-      x += lineLength;
-      e.preventDefault();
+      x += MOVE_AMOUNT;
       break;
     case 'ArrowUp':
-      y += -lineLength;
-      e.preventDefault();
+      y += -MOVE_AMOUNT;
       break;
     case 'ArrowDown':
-      y += lineLength;
-      e.preventDefault();
+      y += MOVE_AMOUNT;
       break;
     default:
       break;
   }
   ctx.lineTo(x, y);
   ctx.stroke();
-});
+}
+
+// when key pressed do a thing - draw a line in the direction
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    // To pass in to options
+    draw({ key: e.key });
+    console.log(e.key);
+    console.log('HANDLING KEY');
+  }
+}
 
 // clear the canvas
-//
-shakeButton.addEventListener('click', function(e) {
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-});
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function() {
+      console.log('done the shake');
+      canvas.classList.remove('shake');
+    },
+    // This removes the event listener after it is run so it doesnt compound
+    { once: true }
+  );
+}
+
+// add event listener for keys pressed and clearing canvas
+
+window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
