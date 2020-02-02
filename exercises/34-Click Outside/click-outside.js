6bdc5c534cce9ef87084f1fa2700333c5a489380
Leanne <leanne.thng@gmail.com>
2020-02-02 14:18:04 -0800

    modal project: set up basic open and close functionality

--- exercises/34-Click Outside/click-outside.js
+++ exercises/34-Click Outside/click-outside.js
@@ -1,8 +1,26 @@
-// Target the button element in the DOM
+// Defining Variables
+// -- Target the button element in the DOM
+const cardButton = document.querySelectorAll('.card button');
+// -- Target the modal window in the Dom
+const modalOuter = document.querySelector('.modal-outer');
 
-// Listen for when clicked
-// Then unhide the modal window
+// Functionality for event listeners
+// -- Once button is clicked - unhide the modal window
+function openModal() {
+  console.log('it got clicked');
+  modalOuter.classList.add('open');
+}
+// -- When the modal-outer is clicked - hide them both
+function closeModal() {
+  console.log('it is closed');
+  modalOuter.classList.remove('open');
+}
 
-// Target the modal window in the Dom
-// Listen for when the modal window is active
-// When the modal-outer is clicked - hide them both
+// Event Listeners
+// -- Listen for when the buttons are clicked
+cardButton.forEach(button => {
+  button.addEventListener('click', openModal);
+});
+
+// -- Listen for when the modal window is visible
+modalOuter.addEventListener('click', closeModal);
