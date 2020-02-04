919be60133c4462cd8233c6207e2b81365b0ccb7
leannethng <leanne.thng@gmail.com>
2020-02-04 13:44:25 -0800

    scroll project: added intersectionObserver code

--- exercises/35-Scroll-To-Accept/scroll-to-accept.js
+++ exercises/35-Scroll-To-Accept/scroll-to-accept.js
@@ -1,30 +1,33 @@
-// Wrapping all the code in a function with an if(!terms) statement to check if there are any terms before executing the code. If there are no terms on the page, the JS will not try to keep running the code.
 function scrollToAccept() {
-  const terms = document.querySelector('.terms-and-conditions');
+  // Wrapping all the code in a function with an if(!terms) statement to check if there are any terms before executing the code. If there are no terms on the page, the JS will not try to keep running the code.
 
+  const terms = document.querySelector('.terms-and-conditions');
+  const watch = document.querySelector('.watch');
+  const button = document.querySelector('.accept');
   // Checking of terms, if none it will quit out of this function
 
   if (!terms) {
     return;
   }
 
-  const button = document.querySelector('button');
-  let lastScrollPosition = 0;
-
-  // Old method of finding the end of the scroll
-  function scrollToEnd() {
-    lastScrollPosition = window.scrollY;
-    console.log(lastScrollPosition);
-    if (terms.scrollHeight - terms.clientHeight === terms.scrollTop) {
-      console.log('its at the bottom');
-      return button.removeAttribute('disabled', '');
+  function obCallback(payload) {
+    console.log(payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio >= 0.5) {
+      button.disabled = false;
+      // Stop observing after the event has happened
+      // ob.unobserve(terms.lastElementChild);
+    } else {
+      button.disabled = true;
     }
-    console.log('you cant click');
-    return button.setAttribute('disabled', '');
   }
 
-  terms.addEventListener('scroll', scrollToEnd);
+  // This is a watcher
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 0.5,
+  });
+
+  ob.observe(terms.lastElementChild);
 }
 
-// When using a function you need to run it
 scrollToAccept();
