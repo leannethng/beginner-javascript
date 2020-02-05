bf3d97f089d21973659e162c5b84822ebb2af9d3
leannethng <leanne.thng@gmail.com>
2020-02-05 15:18:25 -0800

    tab project: tabs changing working, need to work on panel showing next

--- exercises/36-Tabs/notes.md
+++ exercises/36-Tabs/notes.md
@@ -27,3 +27,24 @@ First steps:
 // Finding related div could be done by matching the button `id` with the div `aria-labelledby` need to figure out how to do this matching.
 
 // 2. Listen for clicks - probably need to do the forEach loop over the buttons
+
+---
+
+- Tidied up sudo code, refered to divs now as tab panels and the buttons as tab buttons for ease of understanding.
+
+- From reading the MDN doc, it looks like the idea would be to remove or hide everything first then reveal or select the clicked tab button and panle after so have updated that logic in the sudo code.
+
+- Using the code from my modal click outside project, I got a base of logging when a different button was pressed.
+
+- Used `.setAttribute()` to set the `aria-selected` to true or false and put the buttons in a forEach loop to ensure they were all being accessed to set them all to false. [.setAttribute() MDN Doc](https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute)
+
+- For the tabPanels, I was able to hide them all easily enough, however showing the one we want to see isn't working with the current set up.
+
+```
+ tabPanel
+    .querySelector(`#${currentTab.getAttribute('aria-controls')}`)
+    .removeAttribute('hidden');
+
+```
+
+- I realized that as I am using querySelector which looks down the DOM tree, I might need to move a level up so it can look down on the tabPanels. Will try this next.
--- exercises/36-Tabs/tabs.js
+++ exercises/36-Tabs/tabs.js
@@ -1,12 +1,35 @@
-// 1. Grab the buttons using their `[role="tab"]`. This is the common id that can look for all 3 at once
+// 1. Grab the tab buttons using their `[role="tab"]`. This is the common id that can look for all 3 at once
+const tabButton = document.querySelectorAll(`[role="tab"]`);
 
-// 1. Grab the info divs using their `[role="tabpanel"]`
+// 1. Grab the tab panel using their `[role="tabpanel"]`
+const tabPanel = document.querySelectorAll(`[role="tabpanel"]`);
+
 // 3. Functions that run after the event listener triggers.
+function openPanel(e) {
+  // capture current tab button pressed
+  const currentTab = e.currentTarget;
+  console.log(currentTab);
 
-// Button - When a button is pressed set the `aria-selected` on that button to `true` and the others to `false`
+  // Tab Button - When a tab button is pressed set everything to false then set the selected one to `true`
+  tabButton.forEach(button => {
+    button.setAttribute('aria-selected', false);
+  });
+  currentTab.setAttribute('aria-selected', true);
 
-// Divs - When a button is pressed ensure the related div has the attribute `hidden` removed and that the other divs have `hidden` added
+  // Tab Panels - When a tab button is pressed hide all tab panels the ensure the selected tab panel has the attribute `hidden` removed
+  tabPanel.forEach(panel => {
+    panel.setAttribute('hidden', true);
+  });
 
-// Finding related div could be done by matching the button `id` with the div `aria-labelledby` need to figure out how to do this matching.
+  tabPanel
+    .querySelector(`#${currentTab.getAttribute('aria-controls')}`)
+    .removeAttribute('hidden');
 
-// 2. Listen for clicks - probably need to do the forEach loop over the buttons
+  // Finding related tab panel could be done by matching the tab button `id` with the tab panel `aria-labelledby` need to figure out how to do this matching.
+}
+
+// 2. Listen for clicks - probably need to do the forEach loop over the tab buttons
+
+tabButton.forEach(button => {
+  button.addEventListener('click', openPanel);
+});
