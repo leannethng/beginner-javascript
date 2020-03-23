3af696aab6e5633e0061e86e84e0c0dd9b6823f2
leannethng <leanne.thng@gmail.com>
2020-03-23 14:56:15 -0700

    Module 10: Sarcastic Text

--- exercises/56-Sarcastic Text/text.js
+++ exercises/56-Sarcastic Text/text.js
@@ -1,28 +1,63 @@
 // Grab the dom elements we need ot work with
 const textArea = document.querySelector('[name="text"]');
 const result = document.querySelector('.result');
-const filterInputs = document.querySelectorAll('[name="filter"]');
+const filterInputs = Array.from(document.querySelectorAll('[name="filter"]'));
 
+/* eslint-disable */
+const funkyLetters = {
+  '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ'
+};
+/* eslint-enable */
+// Methods stored inside an object to keep them together
 const filters = {
   sarcastic(letter, index) {
-    console.log(letter, index);
+    if(index % 2){
+      return letter.toUpperCase();
+    } 
+    return letter.toLowerCase();
+  },
+  // the longer way to write - funky: function(){}
+  funky(letter) {
+    //first check if there is a funky letter for this case
+    let funkyLetter = funkyLetters[letter];
+    if (funkyLetter) return funkyLetter
+    // if not then check if there is a lower case letter
+    funkyLetter = funkyLetters[letter.toLowerCase()];
+    if (funkyLetter) return funkyLetter
+    return letter;
+    //  if nothing then return the letter
+  },
+  unable(letter) {
+    const random = Math.floor(Math.random()* 3)
+    if(letter === ' ' && random === 2){
+      return '...';
+    }
     return letter;
   },
-  funky() {},
-  unable() {},
 };
 
 // when text input has text ...
 function transformText(text) {
+  const filter = filterInputs.find(input => input.checked).value;
+  // const filter = document.querySelector('[name="filter"]:checked').value;
+
   // take the text and loop over each letter
-  const mod = Array.from(text).map(filters.sarcastic);
-  console.log(mod);
-  result.textContent = text;
+  // Take filter variable instead of hard coding. As filter is a variable and not a method it needs to go in [] to lookup the property.
+  const mod = Array.from(text).map(filters[filter]);
+  // console.log(mod);
+  result.textContent = mod.join('');
 }
+
+
+
 
 // Display the text in the result paragraph
 
 // based on which filter input is selected alter the text
 
 // Handler to output the text
-window.addEventListener('input', e => transformText(e.target.value));
+textArea.addEventListener('input', e => transformText(e.target.value));
+
+filterInputs.forEach(input => input.addEventListener('input', () => {
+  transformText(textArea.value);
+}))
